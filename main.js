49ecd04b408f3c0e735db89f2b2a55230ef7c48f
Olyiona <olyona_010@mail.ru>
2021-02-04 18:59:10 +0200

    DZ JS1

---
+++ main.js
@@ -0,0 +1,266 @@
+// 1) Створити змінні. Присвоїти кожному з них значення: 
+// 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// Вивести кожну змінну за допомогою: console.log , alert, document.write
+// let zminna1 = "hello"
+// let zminna2  = "owu"
+// let zminna3 = "com"
+// let zminna4 = 'ua'
+// let zminnaNumber1 = 1
+// let zminnaNumber2 = 10
+// let zminnaNumber3 = -999
+// let zminnaNumber4 = 123
+// let zminnaNumber5 = 3.14
+// let zminnaNumber6 = 2.7
+// let zminnaNumber7 = 16
+// let zminna5 = true
+// let zminna6 = false
+
+// alert(zminna1)
+// console.log(zminna1)
+// document.write(zminna1)
+
+// alert(zminna2)
+// console.log(zminna2)
+// document.write(zminna2)
+
+// alert(zminna3)
+// console.log(zminna3)
+// document.write(zminna3)
+
+// alert(zminna4)
+// console.log(zminna4)
+// document.write(zminna4)
+
+// alert(zminnaNumber1)
+// console.log(zminnaNumber1)
+// document.write(zminnaNumber1)
+
+// alert(zminnaNumber2)
+// console.log(zminnaNumber2)
+// document.write(zminnaNumber2)
+
+// alert(zminnaNumber3)
+// console.log(zminnaNumber3)
+// document.write(zminnaNumber3)
+
+// alert(zminnaNumber4)
+// console.log(zminnaNumber4)
+// document.write(zminnaNumber4)
+
+// alert(zminnaNumber5)
+// console.log(zminnaNumber5)
+// document.write(zminnaNumber5)
+
+// alert(zminnaNumber6)
+// console.log(zminnaNumber6)
+// document.write(zminnaNumber6)
+
+// alert(zminnaNumber7)
+// console.log(zminnaNumber7)
+// document.write(zminnaNumber7)
+
+// alert(zminna5)
+// console.log(zminna5)
+// document.write(zminna5)
+
+// alert(zminna6)
+// console.log(zminna6)
+// document.write(zminna6)
+
+// 2) Переприсвоїти кожну змінну з завдання вище на будь які довільні значення.
+//   Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+//  zminna1 = "Hi"
+//  zminna2  = "how"
+//  zminna3 = "are"
+//  zminna4 = 'you'
+//  zminnaNumber1 = 15
+//  zminnaNumber2 = 64
+//  zminnaNumber3 = 925
+//  zminnaNumber4 = 3.8
+//  zminnaNumber5 = 34
+//  zminnaNumber6 = 27
+//  zminnaNumber7 = 99
+//  zminna5 = false
+//  zminna6 = 'Irina'
+
+// alert(zminna1)
+// console.log(zminna1)
+// document.write(zminna1)
+
+// alert(zminna2)
+// console.log(zminna2)
+// document.write(zminna2)
+
+// alert(zminna3)
+// console.log(zminna3)
+// document.write(zminna3)
+
+// alert(zminna4)
+// console.log(zminna4)
+// document.write(zminna4)
+
+// alert(zminnaNumber1)
+// console.log(zminnaNumber1)
+// document.write(zminnaNumber1)
+
+// alert(zminnaNumber2)
+// console.log(zminnaNumber2)
+// document.write(zminnaNumber2)
+
+// alert(zminnaNumber3)
+// console.log(zminnaNumber3)
+// document.write(zminnaNumber3)
+
+// alert(zminnaNumber4)
+// console.log(zminnaNumber4)
+// document.write(zminnaNumber4)
+
+// alert(zminnaNumber5)
+// console.log(zminnaNumber5)
+// document.write(zminnaNumber5)
+
+// alert(zminnaNumber6)
+// console.log(zminnaNumber6)
+// document.write(zminnaNumber6)
+
+// alert(zminnaNumber7)
+// console.log(zminnaNumber7)
+// document.write(zminnaNumber7)
+
+// alert(zminna5)
+// console.log(zminna5)
+// document.write(zminna5)
+
+// alert(zminna6)
+// console.log(zminna6)
+// document.write(zminna6)
+
+// 3) За допомогою 3х різних prompt() отримати 3 слова які являються вашими 
+// Імям, По-Батькові та роками.
+//   Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+// let userName= prompt("імя")
+// let userName2= prompt('По батькові')
+// let userAge = prompt ('вік')
+
+// document.write('Вітаю'+userName)
+// document.write(userName2)
+// document.write('Тобі'+ userAge)
+
+// 4) Взяти змінні з завдання вище та зєднати їх в одну змінну (конкатинація).
+
+// let user = 'Вітаю'+userName+userName2+'Тобі'+userAge;
+// document.write(user)
+
+// 5)За допомогою prompt() отримати 3 числа. Привести їх до числа 
+// (Google "String to number JS"). Вивести їх в консоль.
+
+// let number8 = prompt('number')
+// let number9 = prompt('number')
+// let number10 = prompt('number')
+
+// console.log(+number8)
+// console.log(+number9)
+// console.log(+number10)
+
+// 6) За допомогою prompt() отримати 4 числа. Привести їх до числа 
+// за допомогою paeseInt. Додати їх всі мож собою і результат вивести
+//  в консоль.
+
+// let number11 = parseInt(prompt('number'));
+// let number12 = parseInt(prompt('number'));
+// let number13 = parseInt(prompt('number'));
+// let number14 = parseInt(prompt('number'));
+
+// let sum=(number11+number12+number13+number14);
+
+// console.log(sum)
+
+//  7) За допомогою prompt() отримати 3 числа з плаваючою точкою 
+//за допомогою parseFloat. 
+//   Просумувати їх. Отрманий результат записати в змінну result. 
+//Вивести цю змінну в консоль.
+
+// let number15 = parseFloat(prompt('number'));
+// let number16 = parseFloat(prompt('number'));
+// let number17 = parseFloat(prompt('number'));
+// let number18 = parseFloat(prompt('number'));
+
+// let result = (number15+number16+number17+number18)
+// console.log(result)
+
+// 8) За допомогою prompt() отримати 3 числа з плаваючою точкою.
+//  Заокруглити їх (Google "round number JS"). Просумувати їх між собою. 
+//  Результат вивести в консоль.
+
+// let number19 = parseFloat(prompt('number'))
+// let number20 = parseFloat(prompt('number'))
+// let number21 = parseFloat(prompt('number'))
+
+// let result1 = Math.round(number19+number20+number21)
+// console.log(result1)
+
+// 9) За допомогою prompt() отримати 2 цілих числа.
+//   Перше число - це число яке будуть підносити до степеня. 
+//   Друге число - це число яке являється степенню.
+//   За допомогою Math.pow (Google) піднести перше число в степінь 
+//   другого числа.
+//   Example. Число 1 = 5. Число 2 = 2. Результат = 25 
+
+// let number22 = +(prompt('number'))
+// let number23 = +(prompt('number'))
+// let result3 = Math.pow(number22,number23)
+// console.log(result3)
+
+// 10) За допомогою оператора typeof визначити типи наступних змінних та 
+// вивести їх в консоль.
+// let a = 100;
+// let b = '100'; 
+// let c = true;
+// console.log(typeof(a))
+// console.log(typeof(b))
+// console.log(typeof(c))
+
+// 10)- Поставет відповідний оператор в виразах що б вийшов відповідний результат.
+//   В однакових виразаї не використовувати однакові оператори!!!
+
+    // 5 > 6 -> true
+    // 5 < 6 -> false
+    // 5 <= 6 -> false
+    // 5 === 6 -> false
+    // 10 = 10 -> true
+    // 10 == 10 -> true
+    // 10 < 10 -> false
+    // 10 > 10 -> false
+    // 10 ? 10 -> false
+    // 123 != '123' -> false
+    // 123 == '123' -> true
+
+    // =====================
+    // ======ДОП============
+    // ====================
+        
+    // 1. Три різних числа вводяться через prompt().
+    // За допомоги if else вивести іх в порядку зростання. 
+    // (відсортувати по зростанню)
+
+// let number1= prompt('number')
+// let number2= prompt('number')
+// let number3= prompt('number')
+//  if (number1<number2&number2<number3){
+//  document.write(number1+number2+number3)}
+// else if
+//  (number2<number1&number1<number3){
+// document.write(number2+number1+number3)}
+// else if
+// (number3<number2&number2<number1){
+// document.write(number3+number2+number1)}
+// else if
+// (number1<number3&number3<number2){
+// document.write(number1+number3+number2)}
+// else if
+// (number2<number3&number3<number1){
+// document.write(number2+number3+number1)}
+
+
